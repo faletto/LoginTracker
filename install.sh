ed4f436e74510001836b67e9ad43935721f02245
Taiga <95189694+TaigaM123@users.noreply.github.com>
2024-08-07 15:18:42 -0700

    Update install scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y fswebcam python3 python3-tk python3-pip guvcview
-sudo git clone https://github.com/TaigaM123/LoginTracker /usr/local/bin/LoginTracker
+sudo apt install -y fswebcam python3 python3-pip guvcview
+sudo git clone https://github.com/cj-plusplus/LoginTracker /usr/local/bin/LoginTracker
 sudo chmod -R a+rw /usr/local/bin/LoginTracker
 
 touch /usr/local/bin/LoginTracker/spreadsheet_url.txt
@@ -10,10 +10,12 @@ touch /usr/local/bin/LoginTracker/spreadsheet_url.txt
 echo "creating venv..."
 python3 -m venv /usr/local/bin/LoginTracker/.venv
 source /usr/local/bin/LoginTracker/.venv/bin/activate
-pip install gspread
+pip install gspread flask
 
 #set up autolaunch then launch the program
 echo -e "[Desktop Entry]\nName=LoggyTracker\nExec=/usr/local/bin/LoginTracker/.venv/bin/python3 /usr/local/bin/LoginTracker/main.py" | sudo tee /etc/xdg/autostart/LoggyTracker.desktop
 echo -e "#!/bin/bash\n/usr/local/bin/LoginTracker/.venv/bin/python3 /usr/local/bin/LoginTracker/main.py" > ~/Desktop/Login_Tracker
 sudo chmod u+x ~/Desktop/Login_Tracker
 python3 /usr/local/bin/LoginTracker/main.py
+#TODO - autolaunch Chromium in fullscreen
+#TODO - create Chromium shortcut to 127.0.0.1:5000 on desktop
